a518befea5f3895fe82a9fb3e159054b713a7058
BeCharlie32 <BeCharlie32@outlook.com>
2025-07-27 09:48:44 +1000

    This is the first major change, as it released project 1, just a simple JavaScript program that changes some text. Enjoy!

---
+++ index.js
@@ -0,0 +1,16 @@
+function welcome1(){
+    alert("Welcome to Project 1!")
+
+}
+function welcome2(){
+    alert("Welcome to Project 2!")
+}
+function welcome3(){
+    alert("Welcome to Project 3!");
+}
+
+
+function project1(){
+    document.getElementById("para").innerHTML = "Look! You just changed me"
+}
+
